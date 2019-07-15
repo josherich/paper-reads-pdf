d16b2d459816b0927e6afc997aa8cf55afddbf29
josherich <josherichchen@gmail.com>
2019-07-15 13:30:53 -0400

    feat: extract meta

--- README.md
+++ README.md
@@ -4,10 +4,21 @@ pdf pipeline for paper.mindynode.com
 
 ### run
 
+5 steps are inlcuded
+  1. crawl pdfs
+  2. pdf to text
+  3. screenshot thumbs
+  4. analyze tdidf matrix
+  5. extract meta using grobid
+
 ```
 python main.py --page_uri=https://www.hackernewspapers.com/ --depth=2
+
+// skip crawling
+python main.py --page_uri=https://www.hackernewspapers.com/ --depth=2 --skip_crawl
 ```
 
 the code is based on:
   1. [arxiv-sanity-preserver](https://github.com/karpathy/arxiv-sanity-preserver)
   2. [pdf-crawler](https://github.com/SimFin/pdf-crawler)
+  3. [grobid-client-python](https://github.com/kermitt2/grobid-client-python)
--- main.py
+++ main.py
@@ -4,13 +4,20 @@ import crawler
 from parse_pdf_to_text import pdf_to_text
 from urllib.parse import urlparse
 from analyze import analyze
+from thumb_pdf import thumb_pdf
+from grobid.grobid_client import grobid_client
 
 def run(site_uri, depth, output_dir, skip_crawl):
   if not skip_crawl:
     crawler.crawl(url=site_uri, depth=depth, output_dir=output_dir, method="normal")
   name = urlparse(site_uri).netloc
   pdf_to_text(os.path.join(output_dir, name), os.path.join(output_dir, name+'.txt'))
+  thumb_pdf(os.path.join(output_dir, name), os.path.join(output_dir, name+'.thumb'))
   analyze(os.path.join(output_dir, name, 'list.csv'), os.path.join(output_dir, name))
+
+  client = grobid_client(config_path='./grobid/config.json')
+  client.process(os.path.join(output_dir, name), os.path.join(output_dir, name+'.meta'), 20, 'processHeaderDocument', True, True, True, True, False)
+
 
 if __name__ == "__main__":
 
